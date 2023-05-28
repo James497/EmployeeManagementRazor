66cbe36765737c52cad1ec59f0b0756142bbb1b8
James497 <jamesehembrom@gmail.com>
2023-05-28 14:04:49 +0530

    status code 404 handled when requested page is not found

--- EmployeeManagementRazor/Program.cs
+++ EmployeeManagementRazor/Program.cs
@@ -31,6 +31,7 @@ namespace EmployeeManagementRazor
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/NotFound");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
