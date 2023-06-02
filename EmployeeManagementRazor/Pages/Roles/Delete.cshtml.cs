bdab8ee96e887cd9539f40d1c383d127aacff094
James497 <jamesehembrom@gmail.com>
2023-06-02 16:29:49 +0530

    delete roles implemented

---
+++ EmployeeManagementRazor/Pages/Roles/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeManagementRazor.Pages.Roles
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public DeleteModel(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [BindProperty]
+        public DeleteRoleViewModel roleModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+            roleModel = new DeleteRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name!
+            };
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var role = await roleManager.FindByIdAsync(roleModel.Id);
+            if (role == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+            var result = await roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Roles/Index");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return Page();
+        }
+    }
+    public class DeleteRoleViewModel
+    {
+        public string Id { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
