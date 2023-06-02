5f833fa9dbe0a556c53bdfd6b92e1f21d07d4b72
James497 <jamesehembrom@gmail.com>
2023-06-02 16:15:31 +0530

    delete page for users

---
+++ EmployeeManagementRazor/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,69 @@
+using EmployeeManagementRazor.Models;
+using EmployeeManagementRazor.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementRazor.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public DeleteModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
+        public DeleteUserViewModel userModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+            userModel = new DeleteUserViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName!,
+                Email = user.Email!
+            };
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await userManager.FindByIdAsync(userModel.Id);
+            if(user == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+            var result = await userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Users/Index");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return Page();
+        }
+    }
+    public class DeleteUserViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
